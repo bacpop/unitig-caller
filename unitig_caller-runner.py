a124b73fc1e5a396c980ad2266fe4f764680a335
John Lees <lees.john6@gmail.com>
2019-04-24 16:05:40 -0400

    Fix wrapper

---
+++ unitig_caller-runner.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+"""Convenience wrapper for running unitig-caller directly from source tree."""
+
+from unitig_caller.__main__ import main
+
+if __name__ == '__main__':
+    main()
