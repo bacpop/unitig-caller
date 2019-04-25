353b52402e83ae8171931d7a0ad912eadbce46d7
John Lees <lees.john6@gmail.com>
2019-04-25 15:53:20 -0400

    Update setup.py and add conda recipe

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
-from setuptools import setup, find_packages
 from codecs import open
 from os import path
 import os
 import re
+import sys
 import io
+import platform
+import subprocess
 
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext
+from distutils.version import LooseVersion
 
 def read(*names, **kwargs):
     with io.open(
@@ -22,16 +27,60 @@ def find_version(*file_paths):
         return version_match.group(1)
     raise RuntimeError("Unable to find version string.")
 
+class CMakeExtension(Extension):
+    def __init__(self, name, sourcedir=''):
+        Extension.__init__(self, name, sources=[])
+        self.sourcedir = os.path.abspath(sourcedir)
+
+class CMakeBuild(build_ext):
+    def run(self):
+        try:
+            out = subprocess.check_output(['cmake', '--version'])
+        except OSError:
+            raise RuntimeError("CMake must be installed to build the following extensions: " +
+                               ", ".join(e.name for e in self.extensions))
+
+        if platform.system() == "Windows":
+            cmake_version = LooseVersion(re.search(r'version\s*([\d.]+)', out.decode()).group(1))
+            if cmake_version < '3.1.0':
+                raise RuntimeError("CMake >= 3.1.0 is required on Windows")
+
+        for ext in self.extensions:
+            self.build_extension(ext)
+
+    def build_extension(self, ext):
+        extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
+        cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
+                      '-DPYTHON_EXECUTABLE=' + sys.executable]
+
+        cfg = 'Debug' if self.debug else 'Release'
+        build_args = ['--config', cfg]
+
+        if platform.system() == "Windows":
+            cmake_args += ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(), extdir)]
+            if sys.maxsize > 2**32:
+                cmake_args += ['-A', 'x64']
+            build_args += ['--', '/m']
+        else:
+            cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
+            build_args += ['--', '-j2']
+
+        env = os.environ.copy()
+        env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(env.get('CXXFLAGS', ''),
+                                                              self.distribution.get_version())
+        if not os.path.exists(self.build_temp):
+            os.makedirs(self.build_temp)
+        subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=self.build_temp, env=env)
+        subprocess.check_call(['cmake', '--build', '.'] + build_args, cwd=self.build_temp)
 
 here = path.abspath(path.dirname(__file__))
-
 
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
     name='unitig_caller',
-    version=find_version("pyseer/__init__.py"),
+    version=find_version("unitig_caller/__init__.py"),
     description='unitig-caller: wrapper around mantis to detect presence of sequence elements',
     long_description=long_description,
     url='https://github.com/johnlees/unitig-caller',
@@ -52,5 +101,8 @@ setup(
         "console_scripts": [
             'unitig_caller = unitig_caller.__main__:main'
             ]
-    }
+    },
+    ext_modules=[CMakeExtension('map_strings')],
+    cmdclass=dict(build_ext=CMakeBuild),
+    zip_safe=False
 )
