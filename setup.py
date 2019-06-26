82ad77fd5941f80f5ce7264abc0497a3d62d4ea9
John Lees <lees.john6@gmail.com>
2019-06-26 10:15:39 -0400

    Add CMake cross-compile conda file

--- setup.py
+++ setup.py
@@ -52,7 +52,8 @@ class CMakeBuild(build_ext):
         extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
         cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
                       '-DPYTHON_EXECUTABLE=' + sys.executable,
-                      '-DCMAKE_VERBOSE_MAKEFILE:BOOL=ON']
+                      '-DCMAKE_VERBOSE_MAKEFILE:BOOL=ON',
+                      '-DCMAKE_TOOLCHAIN_FILE=' + ext.sourcedir +'/cross-linux.cmake']
 
         cfg = 'Debug' if self.debug else 'Release'
         build_args = ['--config', cfg]
