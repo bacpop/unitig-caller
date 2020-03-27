a908358e97431ed0442144ed8a04a1621126d7d0
Sam Horsfield <s.horsfield19@imperial.ac.uk>
2020-03-27 15:44:24 +0000

    Added minimizer option to run_bifrost_build in bifrost.py

--- unitig_caller/bifrost.py
+++ unitig_caller/bifrost.py
@@ -32,7 +32,7 @@ def check_bifrost_version(exe='Bifrost'):
     else:
         return install_error
 
-def run_bifrost_build(in_file, add_in_file = None, out_file, coloured = True, kmer_size=31, threads=1, bifrost_exe='Bifrost'):
+def run_bifrost_build(in_file, add_in_file = None, out_file, coloured = True, kmer_size=31, minimizer_size = 23, threads=1, bifrost_exe='Bifrost'):
     """Runs mantis build on a set of squeakr files.
 
     Args:
@@ -46,8 +46,11 @@ def run_bifrost_build(in_file, add_in_file = None, out_file, coloured = True, km
             Colour compacted DBG
             [default = True]
         kmer_size (int)
-            k-mer size used for DBG construction (maximum 31)
+            k-mer size used for DBG construction (maximum = 31)
             [default = 31]
+        minimizer_size (int)
+            minimizer size used for k-mer hashing (maximum = kmer_size)
+            [default = 21]
         threads (int)
             number of threads to use
             [default = 1]
@@ -75,6 +78,7 @@ def run_bifrost_build(in_file, add_in_file = None, out_file, coloured = True, km
             pass
     bifrost_cmd += " -o " + out_file
     bifrost_cmd += " -k " + str(kmer_size)
+    bifrost_cmd += " -m " + str(minimizer_size)
     bifrost_cmd += " -t " + str(threads)
     if coloured == True:
         bifrost_cmd += " -c"
@@ -83,79 +87,3 @@ def run_bifrost_build(in_file, add_in_file = None, out_file, coloured = True, km
         subprocess.run(bifrost_cmd, shell=True, check=True)
     else:
         return "Please submit input files as 'reads.txt' or 'refs.txt' only"
-
-def run_mantis_index(mantis_dir, num_threads=1, delete_RRR=False, mantis_exe='mantis'):
-    """Runs mantis mst on a built index.
-
-    Args:
-        mantis_dir (str)
-            Location of input directory (-p)
-        num_threads (int)
-            Number of threads to use when counting (-t)
-
-            [default = 1]
-        delete_RRR (bool)
-            Remove the previous color class RRR representation (-d)
-
-            [default = False]
-        mantis_exe (str)
-            Location of mantis executable
-
-            [deafault = 'mantis']
-    """
-    mantis_cmd = mantis_exe + " mst"
-    mantis_cmd += " -p " + mantis_dir + "/"
-    mantis_cmd += " -t " + str(num_threads)
-
-    # Note, delete will fail without trailing slash
-    if delete_RRR:
-        mantis_cmd += " -d"
-    else:
-        mantis_cmd += " -k"
-
-    subprocess.run(mantis_cmd, shell=True, check=True)
-
-def run_mantis_query(query_file, mantis_index, mantis_exe='mantis'):
-    """Runs mantis call. Returns iterable with strains
-    for each input sequence.
-
-    Args:
-        query_file (str)
-            Location of input sequences
-        mantis_index (str)
-            Location of index to search
-        mantis_exe (str)
-            Location of mantis executable
-
-            [deafault = 'mantis']
-    """
-    query_out_file = tempfile.mkstemp()[1]
-
-    mantis_cmd = mantis_exe + " query"
-    mantis_cmd += " -p " + mantis_index
-    mantis_cmd += " -o " + query_out_file
-    mantis_cmd += " " + query_file
-
-    subprocess.run(mantis_cmd, shell=True, check=True)
-
-    max_match = 0
-    samples = []
-    with open(query_out_file, 'r') as query_file:
-        for query_line in query_file:
-            new_seq = re.search(r"^seq\d+\t(\d+)", query_line.rstrip())
-            if new_seq:
-                if max_match > 0:
-                    yield(samples)
-                    samples = []
-                max_match = int(new_seq.group(1))
-            else:
-                (squeakr_file, matches) = query_line.rstrip().split("\t")
-                if int(matches) == max_match:
-                    sample_name = re.search(r"\/(.+?)\.squeakr$", squeakr_file)
-                    if sample_name:
-                        samples.append(sample_name.group(1))
-                    else:
-                        sys.stderr.write("Error matching sample name in " + squeakr_file + "\n")
-
-    yield(samples)
-    os.remove(query_out_file)
