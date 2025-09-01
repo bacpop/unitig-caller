4d1178e4f633f3328d7b64b82dc52c0202d7a572
Samuel Horsfield <shorsfield@hl-codon-08-02.ebi.ac.uk>
2025-09-01 14:50:34 +0100

    Fix: fixes writing of pyseer file

--- src/bifrost.cpp
+++ src/bifrost.cpp
@@ -252,10 +252,10 @@ void query_unitig (const ColoredCDBG<>& ccdbg,
 
         // --- Pyseer output ---
         if (pyseer) {
-            pyseer_out << query;
+            pyseer_out << query + " |";
             for (size_t i = 0; i < query_colours.size(); i++){
                 if (query_colours[i] == 1) {
-                    pyseer_out << "\t" << input_colour_pref[i] + ":1";
+                    pyseer_out << " " << input_colour_pref[i] + ":1";
                 }
             }
             pyseer_out << "\n";
@@ -328,10 +328,10 @@ void call_unitigs(const ColoredCDBG<>& ccdbg,
 
         // --- Pyseer output ---
         if (pyseer) {
-            pyseer_out << um_seq;
+            pyseer_out << um_seq + " |";
             for (size_t i = 0; i < um_colours.size(); i++){
                 if (um_colours[i] == 1) {
-                    pyseer_out << "\t" << input_colour_pref[i] + ":1";
+                    pyseer_out << " " << input_colour_pref[i] + ":1";
                 }
             }
             pyseer_out << "\n";
